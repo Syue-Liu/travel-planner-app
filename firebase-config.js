c1594766699a3cad9bf704cf549455a648f7d6db
Syue-Liu <brian963852741@gmail.com>
2026-05-02 14:31:51 +0800

    Enable Firebase trip sync

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.TRAVEL_APP_FIREBASE = {
+  apiKey: "AIzaSyB2HbT6bsE8pspI1wl4CB20PI65obeZlZ8",
+  authDomain: "travel-planner-app-704c0.firebaseapp.com",
+  projectId: "travel-planner-app-704c0",
+  storageBucket: "travel-planner-app-704c0.firebasestorage.app",
+  messagingSenderId: "175780108662",
+  appId: "1:175780108662:web:7fb79300dcc474198c8257",
+  measurementId: "G-BKFZ9JHM36"
+};
